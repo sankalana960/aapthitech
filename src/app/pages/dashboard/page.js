714c74eb6044d2eb521b97ce2380f081434954d2
sankalana960 <sankalana960@gmail.com>
2025-01-20 13:06:46 +0530

    dashboard frontend

---
+++ src/app/pages/dashboard/page.js
@@ -0,0 +1,48 @@
+import "../dashboard/dashboard.css"
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default function Page() {
+    
+  return (
+    <div className="">
+        <nav className='nav-bar'>
+            <p className="nav-menu">Home</p>
+            <p className="nav-menu">User</p>
+        </nav>
+        <div className="containers">
+            <h2 className="user-text">Users</h2>
+            <button className="btn btn-success">Add User</button>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Mobile Number</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>sankalana</td>
+                        <td>chary</td>
+                        <td>sankalana3@gmail.com</td>
+                        <td>8333881112</td>
+                    </tr>
+                    <tr>
+                        <td>sankalana</td>
+                        <td>chary</td>
+                        <td>sankalana3@gmail.com</td>
+                        <td>8333881112</td>
+                    </tr>
+                    <tr>
+                        <td>sankalana</td>
+                        <td>chary</td>
+                        <td>sankalana3@gmail.com</td>
+                        <td>8333881112</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+  )
+}
